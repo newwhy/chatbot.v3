960ccf77c3e573c3b221acd12e079e38bc19f99e
Charlie Wi <hw2425@columbia.edu>
2017-10-14 21:32:16 +0900

    Initial commit

---
+++ meal/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+# Create your views here.
+
+
+def keyboard(request):
+
+    return JsonResponse({
+        'type': 'buttons',
+        'buttons': ['상록원', '그루터기', '아리수', '기숙사식당', '교직원식당']
+
+})
